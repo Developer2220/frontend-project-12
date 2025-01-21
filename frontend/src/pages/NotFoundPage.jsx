e94904a6015bde779eda68a007e3e32751494157
Pavel <web.js.ezh@gmail.com>
2025-01-21 17:17:03 +0300

    refactor(NotFoundPage): move to components react-bootstrap

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,24 +1,27 @@
 import { useTranslation } from "react-i18next";
+import { Container, Image } from "react-bootstrap";
 import NavBar from "../components/NavBar";
 
 const NotFoundPage = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
+
   return (
     <div className="d-flex flex-column h-100">
       <NavBar />
-      <div className="text-center">
-        <img
-          alt={t('notFoundPage.title')}
+      <Container className="text-center">
+        <Image
+          alt={t("notFoundPage.title")}
           className="img-fluid h-25"
           src="/src/assets/Search.svg"
-        ></img>
-        <h1 className="h4 text-muted">{t('notFoundPage.title')}</h1>
+        />
+        <h1 className="h4 text-muted mt-4">{t("notFoundPage.title")}</h1>
         <p className="text-muted">
-        {t('notFoundPage.nav')} <a href="/">{t('notFoundPage.link')}</a>
+          {t("notFoundPage.nav")} <a href="/">{t("notFoundPage.link")}</a>
         </p>
-      </div>
+      </Container>
     </div>
   );
 };
 
 export default NotFoundPage;
+
