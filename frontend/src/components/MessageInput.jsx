13ccfab2c4e834b9b354e2a7a103bef71e212dc7
Pavel <web.js.ezh@gmail.com>
2025-01-23 11:05:29 +0300

    refactor: remove unused slices in channelsSlices,add disables to button in mesageInput

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -59,7 +59,7 @@ const MessageInput = () => {
             value={formData.body}
             onChange={handleChange}
           />
-          <Button type="submit" variant="light">
+          <Button type="submit" variant="light" disabled={!formData.body.trim()}> 
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 16 16"
--- frontend/src/store/slices/channelsSlices.js
+++ frontend/src/store/slices/channelsSlices.js
@@ -1,9 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  channels: [],
-  isLoading: false,
-  error: null,
   currentChannel: {id: '1', name: 'general', removable: false}
 };
 
@@ -11,29 +8,14 @@ const channelsSlices = createSlice({
   name: "channels",
   initialState,
   reducers: {
-    setChannels: (state, action) => {
-      state.channels = action.payload;
-    },
-    setError: (state, action) => {
-      state.error = action.payload;
-      state.isLoading = false;
-    },
-    setLoading: (state) => {
-      state.isLoading = true;
-    },
-
     setCurrentChannel: (state, action)=> {
         state.currentChannel = action.payload;
     }
   },
 });
 
-const selectChannels = (state) => state.channels.channels;
-const selectLoading = (state) => state.channels.isLoading;
-const selectError = (state) => state.channels.error;
-
 const selectCurrentChannel = (state) => state.channels.currentChannel;
 
-export const { setChannels, setCurrentChannel, setLoading, setError } = channelsSlices.actions;
+export const { setCurrentChannel } = channelsSlices.actions;
 export default channelsSlices.reducer;
-export { selectChannels, selectCurrentChannel, selectLoading, selectError };
+export { selectCurrentChannel };
